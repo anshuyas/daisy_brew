123dbb0eb96feb67f00cbee2f3f15ee93bd53031
anshuyas <anshuyasayaju17@gmail.com>
2025-12-05 17:42:38 +0545

    added onboardingscreen1

---
+++ lib/screens/onboarding1_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class OnboardingScreen1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.local_cafe, size: 100, color: Colors.brown),
+            SizedBox(height: 20),
+            Text(
+              'Welcome to DaisyBrew',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 12),
+            Text(
+              'Order delicious drinks with personalized options.',
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
