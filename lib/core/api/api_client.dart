82276e81a3967fc62ff377296717bab49d3e6276
anshuyas <anshuyasayaju17@gmail.com>
2026-01-14 20:51:03 +0545

    added api client and endpoints

---
+++ lib/core/api/api_client.dart
@@ -0,0 +1,53 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+import 'api_endpoints.dart';
+
+final apiClientProvider = Provider<ApiClient>((ref) {
+  return ApiClient();
+});
+
+class ApiClient {
+  late final Dio dio;
+  static const _storage = FlutterSecureStorage();
+
+  ApiClient() {
+    dio = Dio(
+      BaseOptions(
+        baseUrl: ApiEndpoints.baseUrl,
+        connectTimeout: ApiEndpoints.connectionTimeout,
+        receiveTimeout: ApiEndpoints.receiveTimeout,
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
+      ),
+    );
+
+    dio.interceptors.add(
+      InterceptorsWrapper(
+        onRequest: (options, handler) async {
+          final token = await _storage.read(key: 'auth_token');
+          if (token != null) {
+            options.headers['Authorization'] = 'Bearer $token';
+          }
+          handler.next(options);
+        },
+        onError: (error, handler) async {
+          if (error.response?.statusCode == 401) {
+            await _storage.delete(key: 'auth_token');
+          }
+          handler.next(error);
+        },
+      ),
+    );
+
+    if (kDebugMode) {
+      dio.interceptors.add(
+        PrettyDioLogger(requestBody: true, responseBody: true),
+      );
+    }
+  }
+}
---
+++ lib/core/api/api_endpoints.dart
@@ -0,0 +1,13 @@
+class ApiEndpoints {
+  ApiEndpoints._();
+
+  static const String baseUrl = 'http://10.0.2.2:3000/api/v1';
+
+  static const Duration connectionTimeout = Duration(seconds: 30);
+  static const Duration receiveTimeout = Duration(seconds: 30);
+
+  static const String users = '/users';
+  static const String userRegister = '/users/register';
+  static const String userLogin = '/users/login';
+  static String userById(String id) => '/users/$id';
+}
